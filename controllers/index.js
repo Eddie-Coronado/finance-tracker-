c07abea5cf386b7b68bdff00f4d847010359fdc0
Eddie Coronado <coronadoeddiej@gmail.com>
2021-05-15 09:57:35 -0700

    update

--- controllers/index.js
+++ controllers/index.js
@@ -1,22 +1,9 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
 
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
-const sequelize = require('../config/connection');
-const seedFinance = require('../seeds/financeData');
-const seedSpending = require('../seeds/SpendingData');
-
-const seedAll = async () => {
-  await sequelize.sync({ force: true });
-
-  await seedFinance();
-
-  await seedSpending();
-
-  process.exit(0);
-};
-
-seedAll();
